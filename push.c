77c8ef893f91bc31830586f0a0d731a518193e6c
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-20 07:57:26 -0800

    handled when the token is no digit

--- push.c
+++ push.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+#include <ctype.h>
 /**
  * push - Implements the push opcode
  * @stack: A pointer to the top of the stack
@@ -14,7 +15,7 @@ void push(stack_t **stack, unsigned int line_number)
 
 	token = strtok(NULL, " \t\n$");
 
-	if (!token)
+	if (!token || !is_integer(token))
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
@@ -36,4 +37,22 @@ void push(stack_t **stack, unsigned int line_number)
 	(*stack)->prev = new_node;
 
 	*stack = new_node;
+}
+/**
+ * is_integer - Checks if a string represents a valid integer
+ * @str: The string to check
+ *
+ * Return: 1 if the string is a valid integer, 0 otherwise.
+ */
+int is_integer(const char *str)
+{
+	if (!str || *str == '\0' || ((*str == '-' || *str == '+') && *(str + 1) == '\0'))
+		return (0);
+	while (*str)
+	{
+		if (!isdigit(*str) && !(*str == '-' && isdigit(*(str + 1))))
+			return (0);
+		str++;
+	}
+	return (1);
 }
