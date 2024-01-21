b82e597f2a06e542041bf24590642ccbd4483502
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-21 02:43:54 -0800

    if <int> is not an integer or if there is no argument given to push

--- monty.h
+++ monty.h
@@ -57,5 +57,4 @@ void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
 void stack(stack_t **stack, unsigned int line_number);
 void queue(stack_t **stack, unsigned int line_number);
-int is_integer(const char *str);
 #endif /*MONTY_H*/
--- push.c
+++ push.c
@@ -14,15 +14,17 @@ void push(stack_t **stack, unsigned int line_number)
 	stack_t *new_node;
 
 	token = strtok(NULL, " \t\n$");
-
-	if (!token || !is_integer(token))
+	if (!token)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
 	value = atoi(token);
-
+	if (strspn(token, "0123456789-+") != strlen(token))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
@@ -37,22 +39,4 @@ void push(stack_t **stack, unsigned int line_number)
 	(*stack)->prev = new_node;
 
 	*stack = new_node;
-}
-/**
- * is_integer - Checks if a string represents a valid integer
- * @str: The string to check
- *
- * Return: 1 if the string is a valid integer, 0 otherwise.
- */
-int is_integer(const char *str)
-{
-	if (!str || *str == '\0' || ((*str == '-' || *str == '+') && *(str + 1) == '\0'))
-		return (0);
-	while (*str)
-	{
-		if (!isdigit(*str) && !(*str == '-' && isdigit(*(str + 1))))
-			return (0);
-		str++;
-	}
-	return (1);
 }
