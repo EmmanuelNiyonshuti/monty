509ea82a90e1d05e2890052501c14b99fa0c6874
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-21 14:42:47 -0800

    swapping two top elements

--- swap.c
+++ swap.c
@@ -10,7 +10,7 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (!(*stack) || !((*stack)->next))
+	if ((*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
